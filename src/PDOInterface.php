b90f649f6622d63e57e98c75d3e7450a8f330be7
Quentin Renard <qrenard@molotov.tv>
2016-02-18 09:35:58 +0100

    Updated PDOInterface

--- src/PDOInterface.php
+++ src/PDOInterface.php
@@ -4,11 +4,11 @@ namespace Asticode\QueryManager;
 interface PDOInterface
 {
 
-
     /**
-     * @param $sStatement
+     * @param string $statement The SQL statement to prepare for execution.
+     * @param array $options Set these attributes on the returned PDOStatement.
      * @return \PDOStatement
      */
-    public function prepare($sStatement);
+    public function prepare($statement, $options = null);
 
 }
